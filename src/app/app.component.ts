df987a09a398d37789d8ed55864f4652aa885513
Jesus Moreno <jesus.moreno@janaq.com>
2019-12-06 19:42:45 -0500

    Add data sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'layout-material';
+  public listThemes:Array<any>;
+  public listCategories:Array<any>;
+  constructor(){
+  }
+  ngOnInit(){
+    this.listThemes=[
+      {
+        id:1,
+        name:'Filled',
+        icon:'home',
+        link:'/',
+      },
+      {
+        id:2,
+        name:'Outlined',
+        icon:'home',
+        link:'/',
+      },
+      {
+        id:3,
+        name:'Rounded',
+        icon:'home',
+        link:'/',
+      },
+      {
+        id:4,
+        name:'Two-tone',
+        icon:'home',
+        link:'/',
+      },
+      {
+        id:5,
+        name:'Sharp',
+        icon:'home',
+        link:'/',
+      }
+    ];
+    this.listCategories=[
+      {name:'all',value:''},
+      {name:'Action',value:'1'},
+      {name:'Alert',value:'2'},
+      {name:'Av',value:'3'},
+      {name:'Communication',value:'4'},
+    ]
+  }
 }
